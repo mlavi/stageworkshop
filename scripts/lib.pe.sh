7a5f3922b00138b9b90992e998f6f996ca2d9858
stevenpan <stevenpan@stevenpans-MacBook-Pro.local>
2019-04-02 20:15:43 +0800

    drop AUTODC_REPOS in lib.pe.sh

--- scripts/lib.pe.sh
+++ scripts/lib.pe.sh
@@ -51,15 +51,6 @@ function authentication_source() {
         _autodc_restart="sleep 2 && service ${_autodc_service} stop && sleep 5 && service ${_autodc_service} start"
          _autodc_status="service ${_autodc_service} status"
         _autodc_success=' * status: started'
-
-        # REVIEW: override global.vars
-        export AUTODC_REPOS=(\
-         'http://10.132.128.50:81/share/saved-images/autodc-2.0.qcow2' \
-         'nfs://pocfs.nutanixdc.local/images/CorpSE_Calm/autodc-2.0.qcow2' \
-        # 'smb://pocfs.nutanixdc.local/images/CorpSE_Calm/autodc-2.0.qcow2' \
-         'https://s3.amazonaws.com/get-ahv-images/AutoDC2.qcow2' \
-         'http://10.59.103.143:8000/autodc-2.0.qcow2' \
-        )
       fi
 
       dns_check "dc${_autodc_index}.${AUTH_FQDN}"
