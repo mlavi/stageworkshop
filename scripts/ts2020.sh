d60fb2d29cdc58a6a261102e20b7f2f893b88304
Nathan Cox <jncox@users.noreply.github.com>
2020-02-19 22:42:17 -0800

    Temp comment out of Oracle Images while DFS catches up

--- scripts/ts2020.sh
+++ scripts/ts2020.sh
@@ -105,16 +105,16 @@ case ${1} in
       Linux_ToolsVM.qcow2 \
       move-3.4.1.qcow2 \
       MSSQL-2016-VM.qcow2 \
-      GTSOracle/19c-april/19c-bootdisk.qcow2 \
-      GTSOracle/19c-april/19c-disk1.qcow2 \
-      GTSOracle/19c-april/19c-disk2.qcow2 \
-      GTSOracle/19c-april/19c-disk3.qcow2 \
-      GTSOracle/19c-april/19c-disk4.qcow2 \
-      GTSOracle/19c-april/19c-disk5.qcow2 \
-      GTSOracle/19c-april/19c-disk6.qcow2 \
-      GTSOracle/19c-april/19c-disk7.qcow2 \
-      GTSOracle/19c-april/19c-disk8.qcow2 \
-      GTSOracle/19c-april/19c-disk9.qcow2 \
+      #GTSOracle/19c-april/19c-bootdisk.qcow2 \
+      #GTSOracle/19c-april/19c-disk1.qcow2 \
+      #GTSOracle/19c-april/19c-disk2.qcow2 \
+      #GTSOracle/19c-april/19c-disk3.qcow2 \
+      #GTSOracle/19c-april/19c-disk4.qcow2 \
+      #GTSOracle/19c-april/19c-disk5.qcow2 \
+      #GTSOracle/19c-april/19c-disk6.qcow2 \
+      #GTSOracle/19c-april/19c-disk7.qcow2 \
+      #GTSOracle/19c-april/19c-disk8.qcow2 \
+      #GTSOracle/19c-april/19c-disk9.qcow2 \
       Windows2012R2.qcow2 \
       HYCU/Mine/HYCU-4.0.3-Demo.qcow2 \
     )
