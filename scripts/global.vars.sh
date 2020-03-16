01997d8cb96509789d64f9fbe1231367e813617a
Nathan Cox <jncox@users.noreply.github.com>
2020-03-16 00:30:55 -0700

    Update global.vars.sh
    Update for Objects offline Repo

--- scripts/global.vars.sh
+++ scripts/global.vars.sh
@@ -148,7 +148,8 @@ case "${OCTET[3]}" in
 esac
 
 # Stuff needed for object_store
-OBJECTS_OFFLINE_REPO='http://10.42.194.11/workshop_staging/objects'
+OBJECTS_OFFLINE_REPO='http://10.42.38.10/images/'
+#OBJECTS_OFFLINE_REPO='http://10.42.194.11/workshop_staging/objects'
 VLAN=${OCTET[2]}
 NETWORK="${OCTET[0]}.${OCTET[1]}"
 
@@ -206,7 +207,8 @@ case "${OCTET[0]}.${OCTET[1]}" in
     PC_DATA='http://10.55.76.10/seedPC.zip'
     BLUEPRINT_URL='http://10.55.251.38/workshop_staging/CalmBlueprints/'
     DNS_SERVERS='10.55.251.10,10.55.251.11'
-	  OBJECTS_OFFLINE_REPO='http://10.55.251.38/workshop_staging/objects'
+	  #OBJECTS_OFFLINE_REPO='http://10.55.251.38/workshop_staging/objects'
+    OBJECTS_OFFLINE_REPO='http://10.55.76.10/'
     ;;
   10.42 ) # HPOC us-west = PHX
     PC_DEV_METAURL='http://10.42.194.11/workshop_staging/pcdeploy-5.11.2.1.json'
