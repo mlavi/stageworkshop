10aa86626247f3c4acbb56066b84ae7d41da4400
Nathan Cox <jncox@users.noreply.github.com>
2020-04-21 15:24:26 -0700

    Update era_mssql_bootcamp.sh

--- scripts/era_mssql_bootcamp.sh
+++ scripts/era_mssql_bootcamp.sh
@@ -137,8 +137,6 @@ case ${1} in
     && images \
     && flow_enable \
     && pc_cluster_img_import \
-    #&& upload_era_calm_blueprint \
-    #&& sleep 300 \
     && configure_era \
     && prism_check 'PC'
 
