f9d1b75f60f067b9a8ecc37e67e2160906210d5f
Nathan Cox <nathan.cox@nutanix.com>
2020-06-04 21:54:44 -0700

    Updates for Objects SNC

--- scripts/citrix_bootcamp.sh
+++ scripts/citrix_bootcamp.sh
@@ -75,10 +75,10 @@ case ${1} in
   PC | pc )
     . lib.pc.sh
 
-    export BUCKETS_DNS_IP="${IPV4_PREFIX}.16"
-    export BUCKETS_VIP="${IPV4_PREFIX}.17"
-    export OBJECTS_NW_START="${IPV4_PREFIX}.18"
-    export OBJECTS_NW_END="${IPV4_PREFIX}.21"
+    #export BUCKETS_DNS_IP="${IPV4_PREFIX}.16"
+    #export BUCKETS_VIP="${IPV4_PREFIX}.17"
+    #export OBJECTS_NW_START="${IPV4_PREFIX}.18"
+    #export OBJECTS_NW_END="${IPV4_PREFIX}.21"
 
     export _prio_images_arr=(\
             Windows2016.qcow2 \
